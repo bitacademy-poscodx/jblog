05b41b0f7f9e79c5e31265c69e4d156a270752d5
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-18 18:00:03 +0900

    jblog optional example~

--- jblog3/src/main/java/com/poscodx/jblog/controller/BlogController.java
+++ jblog3/src/main/java/com/poscodx/jblog/controller/BlogController.java
@@ -1,20 +1,39 @@
 package com.poscodx.jblog.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/{id:(?!assets).*}")
 public class BlogController {
 	
-	@RequestMapping({"", "/{categoryNo}", "/{categoryNo}/{postNo}" })
+	@ResponseBody
+	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}" })
 	public String index(
 		@PathVariable("id") String id,
-		@PathVariable("categoryNo") Long categoryNo,
-		@PathVariable("postNo") Long postNo) {
-
-		return "blog/main";
+		@PathVariable("pathNo1") Optional<Long> pathNo1,
+		@PathVariable("pathNo2") Optional<Long> pathNo2) {
+		
+		Long categoryNo = 0L;
+		Long postNo = 0L;
+		
+		if(pathNo2.isPresent()) {
+			categoryNo = pathNo1.get();
+			postNo = pathNo2.get();
+		} else if(pathNo1.isPresent()) {
+			categoryNo = pathNo1.get();
+		}
+		
+		// categoryNo == 0일때 기본 categoryNo 세팅
+		
+		// postNo == 0일때 기본 postNo 세팅
+		
+		return "BlogController.index(" + id + ", " + categoryNo + ", " + postNo + ")";
+	
 	}
 	
 	// @Auth
